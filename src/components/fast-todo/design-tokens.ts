c95461a0379aa9d11f19167b3c36687727760190
MiKyung2 <123jmk@naver.com>
2024-01-12 14:30:07 +0900

    레거시 삭제

--- src/components/fast-todo/design-tokens.ts
+++
@@ -1,7 +0,0 @@
-import { DesignToken } from '@microsoft/fast-foundation';
-
-export const fontFamily =
-  DesignToken.create<string>('font-family').withDefault('');
-
-export const neutralForeground =
-  DesignToken.create<string>('neutral-foreground').widthDefault('#2B2B2B');
--- src/components/fast-todo/todo-app/todo-app.template.ts
+++
@@ -1,9 +0,0 @@
-import { html } from '@microsoft/fast-element';
-import type { TodoForm } from '../todo-form/todo-form';
-
-export const template = html<TodoForm>`
-  <form @submit=${x => x.submitTodo()}>
-    <input type="text" />
-    <button type="submit" ?disabled=${x => !x.description}>Add Todo</button>
-  </form>
-`;
